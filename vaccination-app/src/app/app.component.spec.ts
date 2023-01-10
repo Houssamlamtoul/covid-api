3398f66dda348199f292e1793b768cbcd43ebf26
marwane <71854117+Erwen1@users.noreply.github.com>
2023-01-10 17:56:06 +0100

    Merge branch 'main' of https://github.com/Houssamlamtoul/covid-api

--- vaccination-app/src/app/app.component.spec.ts
+++ vaccination-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FrontProjet5A'`, () => {
+  it(`should have as title 'vaccination-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FrontProjet5A');
+    expect(app.title).toEqual('vaccination-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FrontProjet5A app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('vaccination-app app is running!');
   });
 });
--- vaccination-app/src/app/inscription/inscription.component.ts
+++ vaccination-app/src/app/inscription/inscription.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inscription',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) {}
 
   ngOnInit(): void {
+  }
+
+  goToInscription() {
+    this.router.navigate(["/inscription"])
   }
 
 }
